2036e3edcc61fabfcd44a8f987234159678c8d69
debjani1413 <deydebjani916@gmail.com>
2022-01-03 16:50:55 +0530

    Updates

---
+++ precache-manifest.e78c53e0eea2c98cb01af201a116e8da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2e483ff379d2160409745bf747c8e0",
+    "url": "/Weather-Finder/index.html"
+  },
+  {
+    "revision": "f412f9af27d76bb12489",
+    "url": "/Weather-Finder/static/css/2.66eda4fc.chunk.css"
+  },
+  {
+    "revision": "7d39f39337a7e523ecbc",
+    "url": "/Weather-Finder/static/css/main.a5a73ebe.chunk.css"
+  },
+  {
+    "revision": "f412f9af27d76bb12489",
+    "url": "/Weather-Finder/static/js/2.9b0d9541.chunk.js"
+  },
+  {
+    "revision": "7d39f39337a7e523ecbc",
+    "url": "/Weather-Finder/static/js/main.070ceb9c.chunk.js"
+  },
+  {
+    "revision": "2f03ab235f063b28d8d6",
+    "url": "/Weather-Finder/static/js/runtime~main.1c4152be.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Weather-Finder/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Weather-Finder/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Weather-Finder/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Weather-Finder/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/Weather-Finder/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
